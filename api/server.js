b58e7b9a6b7fd7c609b8dbe6fbc1b64c75733904
MsMauraJones <ms.maura.jones@gmail.com>
2023-04-21 06:35:39 -0700

    chore:add api file structure

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ const Role = db.role;
 const Ministry = db.ministry;
 
 
-mongoose.connect(DB_CONNECTION, {
+mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
